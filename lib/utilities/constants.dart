8812cb42b51b40e5516db8b724da447cfe111de6
Ajay Prabhakar <48018942+Chromicle@users.noreply.github.com>
2019-12-25 23:52:55 +0530

    Improvements in Login screen (#2)
    * login_screen: improvements in login screen
    
    * login_screen: minor changes

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+final kHintTextStyle = TextStyle(
+  color: Colors.black54,
+  fontFamily: 'OpenSans',
+);
+
+final kLabelStyle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'OpenSans',
+);
+
+final kBoxDecorationStyle = BoxDecoration(
+  color: Colors.black12,
+  borderRadius: BorderRadius.circular(10.0),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.white,
+      blurRadius: 6.0,
+      offset: Offset(0, 2),
+    ),
+  ],
+);
