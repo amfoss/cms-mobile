4017395b95ddc2a9cb71f8b7e3a1f7e21240653f
Yash Khare <yashsja@gmail.com>
2020-04-05 13:16:58 +0530

    Fix #4 Add date picker for chosing custom dates

--- lib/screens/attendance.dart
+++ lib/screens/attendance.dart
@@ -8,29 +8,20 @@ class Attendance extends StatefulWidget {
 }
 
 class _Attendance extends State<Attendance> {
-
-  final String query = '''
-                      query {
-                      dailyAttendance(date: "${DateFormat("yyyy-MM-dd").format(DateTime.now())}") {
-                                date
-                                membersPresent {
-                                  member {
-                                    username
-                                    fullName
-                                    avatar {
-                                      githubUsername
-                                    }
-                                  }
-                                  duration
-                                }
-                              }
-                      }''';
+  DateTime selectedDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Attendence: ${DateFormat("yyyy-MM-dd").format(selectedDate)}"),
+        leading: new IconButton(
+          icon: new Icon(Icons.calendar_today),
+          onPressed: () => _selectDate(context),
+        ),
+      ),
       body: Query(
-        options: QueryOptions(documentNode: gql(query)),
+        options: QueryOptions(documentNode: gql(_buildQuery())),
         builder: (QueryResult result,
             {VoidCallback refetch, FetchMore fetchMore}) {
           if (result.loading) {
@@ -56,7 +47,8 @@ class _Attendance extends State<Attendance> {
     return ListView.separated(
         itemCount: membersPresent.length,
         itemBuilder: (context, index) {
-          String url = attendance['membersPresent'][index]['member']['avatar']['githubUsername'];
+          String url = attendance['membersPresent'][index]['member']['avatar']
+              ['githubUsername'];
           if (url == null) {
             url = 'github';
           }
@@ -64,15 +56,48 @@ class _Attendance extends State<Attendance> {
             leading: new CircleAvatar(
               radius: 30,
               backgroundColor: Colors.grey,
-              backgroundImage: NetworkImage(
-                  'https://avatars.githubusercontent.com/' + url),
+              backgroundImage:
+                  NetworkImage('https://avatars.githubusercontent.com/' + url),
             ),
-            title: Text(
-                attendance['membersPresent'][index]['member']['fullName']),
+            title:
+                Text(attendance['membersPresent'][index]['member']['fullName']),
             subtitle: Text(
                 "duration: " + attendance['membersPresent'][index]['duration']),
           );
         },
         separatorBuilder: (context, index) => Divider());
+  }
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2018, 1),
+        lastDate: DateTime.now());
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+    build(context);
+  }
+
+  String _buildQuery() {
+    String query = '''
+                      query {
+                      dailyAttendance(date: "${DateFormat("yyyy-MM-dd").format(selectedDate)}") {
+                                date
+                                membersPresent {
+                                  member {
+                                    username
+                                    fullName
+                                    avatar {
+                                      githubUsername
+                                    }
+                                  }
+                                  duration
+                                }
+                              }
+                      }''';
+    return query;
   }
 }
