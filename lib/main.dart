0520ca1ba4ffbd274a4b91b5b736a1953f13ae60
theamankumarsingh <amankumarsingh.professional@gmail.com>
2021-06-27 03:45:20 +0530

    fix: force app orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:cms_mobile/appInit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(MyApp());
 }
